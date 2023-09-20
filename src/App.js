1f8b08a931b3f32da7b9467580294ebc72091dff
duynn22 <duynn22@viettel.com.vn>
2023-09-20 14:29:35 +0700

    return default

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import _4k_image from './images/pexels-francesco-ungaro-1525041.jpg';
+import logo from './logo.svg';
+// import _4k_image from './images/pexels-francesco-ungaro-1525041.jpg';
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //       <img src={_4k_image} className={'image'} alt={'images'}/>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-          <img src={_4k_image} className={'image'} alt={'images'}/>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
     </div>
   );
 }
